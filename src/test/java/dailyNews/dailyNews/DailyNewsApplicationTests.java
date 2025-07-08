ce613c9d2721febe6ea7dd02315653db2cb2b968
James Orozco <jamesorozcoh@gmail.com>
2025-07-08 14:19:20 -0500

    Initial commit

---
+++ src/test/java/dailyNews/dailyNews/DailyNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package dailyNews.dailyNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
