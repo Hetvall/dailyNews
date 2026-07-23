a6f523fd35b8becc95cbe85bdfb85c6ad8bdb2ef
hetvall <jamesorozcoh@gmail.com>
2026-07-23 14:41:08 -0500

    refactor: migrate database configuration from MySQL to PostgreSQL; update User entity and application properties

--- src/main/java/dailyNews/dailyNews/entity/User.java
+++ src/main/java/dailyNews/dailyNews/entity/User.java
@@ -1,9 +1,11 @@
 package dailyNews.dailyNews.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
+@Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -24,6 +27,7 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String email;
     private String name;
     private String password;
